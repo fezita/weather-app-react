71709a7088f151b5bb76fb8526c1f5a0a55f20df
fezita <78113405+fezita@users.noreply.github.com>
2021-05-12 17:45:34 +0100

    Added open source link

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ function App() {
         </h1>
         <Weather />
       </header>
+      <small>
+        <a href="https://github.com/fezita/weather-app-react" target="_blank" rel="noreferrer">Open source code</a>
+        ", by"
+        <a href="https://www.linkedin.com/in/fernanda-schmitt-b4a3b733/" target="_blank" rel="noreferrer">Fernanda Schmitt</a>
+      </small>
     </div>
   );
 }
